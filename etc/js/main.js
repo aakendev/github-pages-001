dfab14d0a5cd840aa3ed53d643f7d44420b43c47
aakendev <aakendev@gmail.com>
2021-01-02 13:45:16 +0700

    Adding Menu Bar

--- etc/js/main.js
+++ etc/js/main.js
@@ -1,1 +1,12 @@
-alert("JavaScript Alert");
+var folderIc = document.querySelector("#folder-ic");
+var menuBar = document.querySelector("#menu-bar");
+
+folderIc.onclick = () => {
+    if(menuBar.style.maxHeight){
+        menuBar.style.maxHeight = null;
+        folderIc.className = "fas fa-folder fa-2x";
+    } else {
+        menuBar.style.maxHeight = "500px";
+        folderIc.className = "fas fa-folder-open fa-2x";
+    }
+}
