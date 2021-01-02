75026bbf613d28114a7cc5de3bce5a04ea380ffa
aakendev <aakendev@gmail.com>
2021-01-02 18:25:30 +0700

    Fixing Menu Height

--- etc/js/main.js
+++ etc/js/main.js
@@ -6,7 +6,7 @@ folderIc.onclick = () => {
         menuBar.style.maxHeight = null;
         folderIc.className = "fas fa-folder fa-2x";
     } else {
-        menuBar.style.maxHeight = menuBar.scrollHeight + "px";
+        menuBar.style.maxHeight = "1000px";
         folderIc.className = "fas fa-folder-open fa-2x";
     }
 }
