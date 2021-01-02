0ec5084fc11f79b996a5f1ec27e85a71b8b15df2
aakendev <aakendev@gmail.com>
2021-01-02 21:46:47 +0700

    Adding Quiz, Section

--- etc/js/main.js
+++ etc/js/main.js
@@ -1,5 +1,7 @@
 var folderIc = document.querySelector("#folder-ic");
 var menuBar = document.querySelector("#menu-bar");
+var btnCheck = document.querySelector("#btn-check");
+var trueIcon = document.querySelectorAll(".fa-check-circle");
 
 folderIc.onclick = () => {
     if(menuBar.style.maxHeight){
@@ -9,4 +11,15 @@ folderIc.onclick = () => {
         menuBar.style.maxHeight = "1000px";
         folderIc.className = "fas fa-folder-open fa-2x";
     }
+}
+
+btnCheck.onclick = () => {
+    for(var i = 0; i < trueIcon.length; i++){
+        trueIcon[i].style.color = "#00FF00";
+    }
+    setTimeout(() => {
+        for(var j = 0; j < trueIcon.length; j++){
+            trueIcon[j].style.color = "#B7193D";
+        }
+    }, 3000);
 }
